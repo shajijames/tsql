6c3315806f2d0f1efc22d81493222dc12043e1a1
Shaji James <shajijames7@gmail.com>
2018-12-27 00:29:50 +0530

    Update factorial.sql

--- factorial.sql
+++ factorial.sql
@@ -1,14 +1,14 @@
+CREATE PROCEDURE factorial (@n INT)
+AS
 BEGIN
-        DECLARE @n INT = 4
         DECLARE @fac INT
-
         SET @fac = @n
-
         WHILE @n > 1
         BEGIN
                 SET @fac = @fac * (@n - 1)
                 SET @n = @n - 1
         END
-
         PRINT @fac
 END
+
+EXEC factorial 5
